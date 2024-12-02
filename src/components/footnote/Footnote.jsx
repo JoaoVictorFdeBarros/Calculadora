16cc2fb5303bb5643049788ca2e5d6dd00aefe45
JoaoVictorFdeBarros <JoaoVictorFdeBarros@gmail.com>
2024-12-02 18:46:16 -0300

    update

--- src/components/footnote/Footnote.jsx
+++ src/components/footnote/Footnote.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 export default function Footnote(){
     return(
         <div className='Footnote'>
-            <a href='google.com'><FaGithub /> Repositório</a>
+            <a href='https://github.com/JoaoVictorFdeBarros/Calculadora'><FaGithub /> Repositório</a>
             <p>Desenvolvido por João Victor F. Barros - Dezembro 2024</p>
         </div>
     )
